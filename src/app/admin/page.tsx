64fcbdcf00237d042bd2595b3dcc9289184331ad
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-04-08 11:06:49 -0400

    add dashboard card with formatter functions to represent amounts in USD

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,42 @@
-export default function AdminDashboard() {
-    return <h1>Dashboard</h1>
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import db from "@/db/db";
+import { formatCurrency, formatNumber } from "@/db/formatters";
+
+async function getSalesData(){
+ const data = await db.order.aggregate({
+    _sum: {pricePaidInCents: true},
+    _count: true
+ })   
+
+ return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numberOfSales: data._count
+ }
+}
+
+export default async function AdminDashboard() {
+    const salesData = await getSalesData()
+    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+       <DashboardCard title="Sales" subtitle={formatNumber(salesData.numberOfSales)} body={formatCurrency(salesData.amount)}/>
+       <DashboardCard />
+       <DashboardCard />
+    </div>
+}
+type DashboardCardProps = {
+    title: string,
+    subtitle: string, 
+    body: string
+}
+
+function DashboardCard({title, subtitle, body}: DashboardCardProps) {
+    return <>
+    <Card>
+    <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+        </CardHeader>
+    
+    <CardContent>{body}</CardContent>
+</Card>
+</>
 }
