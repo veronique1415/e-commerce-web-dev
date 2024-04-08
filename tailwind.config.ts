3c591836c34ade06a170e0aa9695dc18012eba98
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-04-08 10:36:04 -0400

    add nav bar to admin page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from "tailwindcss"
+import {fontFamily} from "tailwindcss/defaultTheme"
+
 
 const config = {
   darkMode: ["class"],
@@ -18,6 +20,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)",...fontFamily.sans]
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
